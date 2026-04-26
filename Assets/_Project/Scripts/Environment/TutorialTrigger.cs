020feb3edadb2884540ccbaf26d003050457a64f
Aliz Pasztor <al1zp4sztor@gmail.com>
2026-04-26 19:56:27 +0100

    UI - Tutorial message

---
+++ Assets/_Project/Scripts/Environment/TutorialTrigger.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class TutorialTrigger : MonoBehaviour
+{
+    [SerializeField] private string message = "Use this ability to interact with the world.";
+    [SerializeField] private float displayDuration = 5f;
+    [SerializeField] private bool oneShot = true;
+
+    private bool fired = false;
+
+    private void Awake()
+    {
+        GetComponent<Collider>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (fired || !other.CompareTag("Player")) return;
+
+        GameHUD hud = FindFirstObjectByType<GameHUD>();
+        if (hud == null) return;
+
+        hud.ShowTutorialMessage(message, displayDuration);
+
+        if (oneShot)
+        {
+            fired = true;
+            Destroy(gameObject);
+        }
+    }
+}
