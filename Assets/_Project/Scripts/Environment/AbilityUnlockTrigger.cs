0497a64821022b4aab6bbe974935aefcc1375872
Aliz Pasztor <al1zp4sztor@gmail.com>
2026-04-27 19:18:31 +0100

    Created AbilityUnlockTrigger

---
+++ Assets/_Project/Scripts/Environment/AbilityUnlockTrigger.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class AbilityUnlockTrigger : MonoBehaviour
+{
+    public enum AbilityType { EchoPulse, ResonanceHum }
+
+    [SerializeField] private AbilityType abilityToUnlock = AbilityType.ResonanceHum;
+    [SerializeField] private EchoPulseAbility echoPulse;
+    [SerializeField] private ResonanceHumAbility resonanceHum;
+    [SerializeField] private string unlockMessage = "New ability unlocked!";
+    [SerializeField] private float displayDuration = 6f;
+
+    private void Awake()
+    {
+        GetComponent<Collider>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        GameHUD hud = FindFirstObjectByType<GameHUD>();
+        if (hud == null) return;
+
+        switch (abilityToUnlock)
+        {
+            case AbilityType.EchoPulse:
+                if (echoPulse == null) break;
+                echoPulse.SetLocked(false);
+                hud.UnlockEchoPulse();
+                break;
+
+            case AbilityType.ResonanceHum:
+                if (resonanceHum == null) break;
+                resonanceHum.SetLocked(false);
+                hud.UnlockResonanceHum();
+                break;
+        }
+
+        hud.ShowTutorialMessage(unlockMessage, displayDuration);
+        Destroy(gameObject);
+    }
+}
